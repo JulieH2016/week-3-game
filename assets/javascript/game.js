6c109c49190b852b7e386318ad579ef5f7f6644b
Julie Hoffman <jhoffman2013@gmail.com>
2016-10-24 17:21:39 -0400

    Added javascript game file content

---
+++ assets/javascript/game.js
@@ -0,0 +1,83 @@
+//Global Variables and Arrays to hold data
+
+var winCount = 0;
+var lossCount = 0;
+
+var wordOptions = ["html", "css", "span", "file", "java"];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+var guessesLeft = 10;
+var letterGuessed = [];
+
+//Function Codes to call on later
+
+function startGame () {
+	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.lenght)];
+	lettersinWord = selectedWord.split(**);
+	numBlanks = lettersinWord.length;
+
+	guessesLeft = 10;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+
+	for (var i=0; i<numBlanks; i++){
+		blanksAndSuccesses.push("_");
+}
+
+document.getElementsByID("wordToGuess").innerHTML = blanksAndSuccesses.join("");
+document.getElementsByID("numGuesses").innerHTML = guessesLeft;
+document.getElementsByID("winCounter").innerHTML = winCount;
+document.getElementsByID("lossCounter").innerHTML - lossCount;
+
+console.log(selectedWord);
+console.log(lettersinWord);
+console.log(numBlanks);
+console.log(blanksAndSuccesses);
+}
+
+function checkLetters(letter) {
+
+	var isLetterInWord = false;
+	for (var i=0; i<numBlanks; i++) {
+		if(selectedWord[i] == letter) {
+			is LetterInWord = true;
+		}
+	}
+	if(isLetterInWord) {
+	for (var i=0; i<numBlanks; i++) {
+		if (selectedWord[i] == letter) {
+			blanksAndSuccesses[i] = letter;
+			}
+		}
+	}
+
+	else {
+		wrongLetters.push(letter);
+		guessesLeft ==
+	}
+
+	console.log(blanksAndSuccesses);
+
+	function roundComplete(){
+		console.log("Win Count: "+ winCount +" | Loss Count: "+ lossCount +" | Guesses Left: "+ guessesLeft);
+	}
+
+//Main Process
+
+
+
+
+
+startGame();
+
+document.onkeyup = function(event){
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed);
+	roundComplete();
+
+	console.log(letterGuessed);
+}
